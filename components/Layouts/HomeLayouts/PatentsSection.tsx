33de8a8e46a8faed7bafc2383fc8ed256a2e933c
Yashwin <yashwin.sharma@digixito.org>
2026-06-12 15:03:38 +0530

    initial changes

--- components/Layouts/HomeLayouts/PatentsSection.tsx
+++ components/Layouts/HomeLayouts/PatentsSection.tsx
@@ -16,6 +16,7 @@ export default function PatentsSection() {
         label="Our Young Innovators'"
         title="Patents"
         swiperRef={swiperRef}
+        viewAllHref="/patents-granted"
       />
       <Box sx={{ "& .swiper": { width: "100%" } }}>
         <Swiper
